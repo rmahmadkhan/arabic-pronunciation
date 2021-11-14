a017c5749dd1ba5555734304abfc93e78ab88767
Ahmad Khan <rmahmadkhan@icloud.com>
2021-11-14 15:09:41 +0500

    Added navigations for onTap on Practice and Test butttons

--- app/src/main/java/dev/ahmadkhan/arabic_pronunciation/MainActivity.java
+++ app/src/main/java/dev/ahmadkhan/arabic_pronunciation/MainActivity.java
@@ -2,7 +2,9 @@ package dev.ahmadkhan.arabic_pronunciation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void navigateToPractice(View view) {
+        Intent intent = new Intent(MainActivity.this, PracticeActivity.class);
+        startActivity(intent);
+    }
+
+    public void navigateToTest(View view) {
+        Intent intent = new Intent(MainActivity.this, TestActivity.class);
+        startActivity(intent);
     }
 }
