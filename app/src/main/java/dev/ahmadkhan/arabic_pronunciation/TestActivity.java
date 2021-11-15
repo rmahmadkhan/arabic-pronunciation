67f87c77083e638f3689bf8141d597564aaa7103
Ahmad Khan <rmahmadkhan@icloud.com>
2021-11-15 09:22:07 +0500

    Wrote onClickNext

--- app/src/main/java/dev/ahmadkhan/arabic_pronunciation/TestActivity.java
+++ app/src/main/java/dev/ahmadkhan/arabic_pronunciation/TestActivity.java
@@ -4,16 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class TestActivity extends AppCompatActivity {
 
     String[] letters = {"\u0627","\u0628", "\u062A","\u062B","\u062C","\u062D","\u062E","\u062F","\u0630","\u0631","\u0632","\u0633","\u0634","\u0635","\u0636","\u0637", "\u0638","\u0639","\u063A", "\u0641", "\u0642","\u0643","\u0644", "\u0645", "\u0646","\u0647", "\u0648", "\u064A","\u0622","\u0629","\u0649"};
     String[] emissionPoints= {"Halqiyah","Shafaweeyah","Niteeyah", "Lisaveyah", "ShajariyahHaafiyah", "Halqiyah", "Halqiyah", "Niteeyah", "Lisaveyah", "Thalqeeyah", "Asleeyah", "Asleeyah", "ShajariyahHaafiyah", "Asleeyah", "ShajariyahHaafiyah", "Niteeyah", "Lisaveyah", "Halqiyah","Halqiyah", "Shafaweeyah", "Lahatiyah", "Lahatiyah", "Tarfiyah", "Ghunna", "Halqiyah", "Shafaweeyah", "ShajariyahHaafiyah", "Halqiyah", "Halqiyah", "Halqiyah"};
 
+
+    int index = 0;
+    int score = 0;
+
+    TextView textViewLetter;
+    TextView textViewAnswer;
+    TextView textViewScore;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
+        textViewLetter.setText(letters[index++]);
     }
 
     public void onClickNiteeyah(View view) {
@@ -47,6 +59,13 @@ public class TestActivity extends AppCompatActivity {
     }
 
     public void onClickNext(View view) {
+        textViewScore.setText(Integer.toString(score));
+        if(index > 29){
+            textViewLetter.setText("Questions completed");
+        }
+        else{
+            textViewLetter.setText(letters[index++]);
+        }
     }
 
     public void onClickFinish(View view) {
