02cee7cc0c39ef4c6417e0e58c2bbe9cb9e4a7d2
Ahmad Khan <rmahmadkhan@icloud.com>
2021-11-15 08:55:26 +0500

    Added letters and emissionPoints lists to TestActivity

--- app/src/main/java/dev/ahmadkhan/arabic_pronunciation/TestActivity.java
+++ app/src/main/java/dev/ahmadkhan/arabic_pronunciation/TestActivity.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 
 public class TestActivity extends AppCompatActivity {
 
+    String[] letters = {"\u0627","\u0628", "\u062A","\u062B","\u062C","\u062D","\u062E","\u062F","\u0630","\u0631","\u0632","\u0633","\u0634","\u0635","\u0636","\u0637", "\u0638","\u0639","\u063A", "\u0641", "\u0642","\u0643","\u0644", "\u0645", "\u0646","\u0647", "\u0648", "\u064A","\u0622","\u0629","\u0649"};
+    String[] emissionPoints= {"Halqiyah_b","Shafaweeyah","Niteeyah", "Lisaveyah", "Shajariyah_Haafiyah", "Halqiyah_b", "Halqiyah_b", "Niteeyah", "Lisaveyah", "Thalqeeyah", "Asleeyah", "Asleeyah", "Shajariyah_Haafiyah", "Asleeyah", "Shajariyah_Haafiyah", "Niteeyah", "Lisaveyah", "Halqiyah_b","Halqiyah_b", "Shafaweeyah", "Lahatiyah_b", "Lahatiyah_b", "Tarfiyah", "Ghunna", "Halqiyah_b", "Shafaweeyah", "Shajariyah_Haafiyah", "Halqiyah_b", "Halqiyah_b", "Halqiyah_b"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
